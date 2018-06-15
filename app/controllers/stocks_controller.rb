bd4b6ad84e8f9f7d1e04cb83e2881bb658f4fb6f
Matt <mattblesoe2@gmail.com>
2018-06-15 14:48:00 -0400

    add search stock ajax and errors

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,15 @@
+class StocksController < ApplicationController
+
+  def search
+    if params[:stock].blank?
+      flash.now[:danger] = "You failed to type anything "
+    else
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "You entered a incorrect symbol " unless @stock
+    end
+    respond_to do |format|
+      format.js { render partial: 'users/result' }
+    end
+  end
+
+end
